4f2c69ab0c18d82ad28e9ee6d0d13a5f3048bbe6
Januario95 <januariocipriano@gmail.com>
2021-07-29 02:21:39 +0800

    Make changes to Person class

--- GitForProgrammers/index.py
+++ GitForProgrammers/index.py
@@ -30,17 +30,24 @@ def login(user, passwd):
 
 login(username, password)
 
+
 class Person(object):
     def __init__(self, name=None, age=None):
         self.__name = name
-        self.__age = age 
+        self.__age = age
 
     @property
     def name(self):
-        return self.__name 
+        return self.__name
 
     @name.setter
     def name(self, new_name):
-        self.__name = name
+        self.__name = new_name
 
+    @property
+    def age(self):
+        return self.__age
 
+    @age.setter
+    def age(self, new_age):
+        self.__age = new_age
